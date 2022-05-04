166424563ae9c1d79f15df29a77e4c9ef18ee737
bibadel <nicolasscharre@outlook.fr>
2022-05-04 08:46:35 +0200

    Mise à jour base application

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Document</title>
+    <title>{{ config('app.name') }} | @yield('title')</title>
 </head>
 <body>
-    
+    @include('app.nav')
+    <div class="container">
+        @yield('content')
+    </div>
+ 
+    @include('app.footer')
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
