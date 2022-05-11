5baa9917611901c3d4167a1d514c59e32001b80e
Musubi42 <raphael.dev@protonmail.com>
2022-05-11 13:47:18 +0200

    désactivation Footer et livewire

--- resources/views/component/app.blade.php
+++ resources/views/component/app.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    @livewireStyles
+    {{-- @livewireStyles --}}
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,11 +11,13 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>@yield('title')</title>
 </head>
+
 <body>
     @include('component.nav')
     @yield('content')
-    @include('component.footer')
+    {{-- @include('component.footer') --}}
 
-    @livewireScripts
+    {{-- @livewireScripts --}}
 </body>
+
 </html>
