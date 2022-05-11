c05e7430345d7e1df8ae4071ed1773204c4ce10f
Musubi42 <raphael.dev@protonmail.com>
2022-05-11 13:47:28 +0200

    Stylisation header

--- resources/views/component/nav.blade.php
+++ resources/views/component/nav.blade.php
@@ -1,10 +1,14 @@
-<nav class="navbar navbar-light navbar-expand-md navigation-clean-search" style="color: var(--bs-dark);background: rgb(0,0,0);height: 50px;">
-        <div class="container"><button onclick="window.location.href='{{ route('accueil') }}'" class="btn btn-primary" type="button" style="background: url(&quot;/img/outline_home_white_24dp.png&quot;) center / contain no-repeat;width: 35px;height: 35px;margin: 0px;border-style: none;padding: 0px;"></button><a class="navbar-brand" href="{{ route('accueil') }}" style="color: var(--bs-white);text-align: center;height: 40px;margin: 30px;">Sparcoon</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="navbar-nav"></ul>
-                <form class="me-auto search-form" target="_self">
-                    <div class="d-flex align-items-center"><label class="form-label d-flex mb-0" for="search-field"></label></div>
-                </form><a class="btn btn-light action-button" role="button" href="{{ route('profil') }}" style="background: url(&quot;/img/outline_person_white_24dp.png&quot;) center / contain no-repeat;height: 35px;width: 35px;margin: 0px;padding: 0px;"></a>
-            </div>
+<nav class="bg-black flex justify-between p-6 px-4">
+    <div class="flex justify-between items-center w-full flex-row">
+        <div class="w-1/2 xl:w-1/3 flex">
+            <a class="block max-w-max" href="#">
+                <img class="h-8" src="{{ asset('storage/asset/image/logo_entier.png') }}" alt="">
+            </a>
         </div>
+        <div class="w-1/2 xl:w-1/3 xl:flex xl:justify-end">
+            <a class="block max-w-max" href="#">
+                <img class="h-8" src="{{ asset('storage/asset/image/logo_logo.png') }}" alt="">
+            </a>
+        </div>
+    </div>
 </nav>
