c49a94ce09e4b04dd81edfbe4d12c43af4156441
Musubi42 <raphael.dev@protonmail.com>
2022-05-14 19:05:54 +0200

    Add button ajout objet

--- resources/views/patrimoine/objet_create.blade.php
+++ resources/views/patrimoine/objet_create.blade.php
@@ -169,10 +169,26 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="flex justify-end">
+                    <div
+                        class="inline-flex h-14 w-auto mx-4 items-center justify-start text-white bg-green-700 rounded-lg pr-4">
+                        <div class="mx-4">
+                            <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" fill="white"
+                                viewBox="0 0 448 512">
+                                {{-- ! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --}}
+                                <path
+                                    d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
+                            </svg>
+                        </div>
+                        <span class="whitespace-nowrap font-semibold text-2xl">Ajouter l'objet</span>
+                    </div>
+                </div>
             </section>
 
-            <section>
-                <div id="defaultModal" tabindex="-1" aria-hidden="true"
+
+            <section class="mb-6">
+                <div id=" defaultModal" tabindex="-1" aria-hidden="true"
                     class="hidden overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
                     <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
 
@@ -192,7 +208,8 @@
                                         type="text" placeholder="Product description"></textarea>
                                 </div>
                                 <div class="mb-8">
-                                    <label class="block mb-2 text-coolGray-800 font-medium leading-6" for="">Faites glisser
+                                    <label class="block mb-2 text-coolGray-800 font-medium leading-6" for="">Faites
+                                        glisser
                                         la
                                         photo</label>
                                     <div
