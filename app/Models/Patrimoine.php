e9aea3cb75a7b57b6e89946c72618f0b243e68f4
Musubi42 <raphael.dev@protonmail.com>
2022-05-16 17:26:14 +0200

    Maj only show patrimoine of the user

--- app/Models/Patrimoine.php
+++ app/Models/Patrimoine.php
@@ -2,28 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\objectEstate;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Patrimoine extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
 
-    protected $fillable = [
-        'name',
-        'description',
-        'user_id',
-    ];
+  protected $fillable = [
+    'name',
+    'description',
+    'user_id',
+  ];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 
-    public function objectEstates()
-    {
-        return $this->hasMany(ObjectEstate::class);
-    }
-
+  public function objectEstates()
+  {
+    return $this->hasMany(ObjectEstate::class);
+  }
 }
